2649ebd4849a6f0885d9f566bb4811bf2aaeb8cc
Mark Strefford <markstrefford@me.com>
2016-04-28 17:10:19 +0100

    Random movement of the cab
    Implemented first part of the P4 project by making the agent pick
    random actions for the cab from a defined list
    
    Also implemented the Udacity code in git to enable switching off
    of the simulator for training in future questions

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -11,6 +11,7 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
+        self.actions = None, 'forward', 'left', 'right'
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
@@ -25,7 +26,7 @@ class LearningAgent(Agent):
         # TODO: Update state
 
         # TODO: Select action according to your policy
-        action = None
+        action = random.choice(self.actions)
 
         # Execute action and get reward
         reward = self.env.act(self, action)

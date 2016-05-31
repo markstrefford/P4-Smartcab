582d3b8bc110048b982841dce51c094fb867b98c
Mark Strefford <mark@m20ventures.co.uk>
2016-05-31 16:32:12 +0100

    Working on new state model
    Refactoring code to create specific functions for creating a new state,
    updating / modifying the q-values.
    For now assume that the reward is the q-value that's stored.
    This is based on the examples given in the Udacity forums for P4

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -13,11 +13,10 @@ class LearningAgent(Agent):
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
         self.possible_actions = [None, 'forward', 'left', 'right']
-        self.dict_actions = dict()  # Default dict for q-values
-        self.dict_actions = {'None': self.set_initial_q(),
+        self.init_actions_rewards = dict({'None': self.set_initial_q(),
                              'forward': self.set_initial_q(),
                              'left': self.set_initial_q(),
-                             'right': self.set_initial_q()}
+                             'right': self.set_initial_q()})
         # self.states = self.setup_states()  # TODO - is this needed now?
         self.q = dict()
         self.iteration_0 = True
@@ -30,6 +29,25 @@ class LearningAgent(Agent):
     def set_initial_q(self):
         #TODO - Determine optimal initialisation value
         return 0
+
+    # TODO - Make this work with multiple actions and rewards!
+    def new_state(self, state, structure = {"state": {}, "actions_rewards": {}}):
+        next_entry = len(self.q)
+        self.q[next_entry] = dict(structure)
+        self.q[next_entry]["state"] = state
+        self.q[next_entry]["actions_rewards"] = self.init_actions_rewards()
+        return next_entry
+
+    def set_state(self, id, state):
+        self.q[id]["state"] = state
+
+    def set_action(self, id, state, action, reward):
+        #TODO - Rework this whole thing!!
+        # Needs to find out if we can find a matching state first,
+        # create one if we don't
+        # Set the relevant action's reward (or modify it using q-learning eventually!)
+        self.q[id]["actions_rewards"] = action
+
 
     # 1) Sense the environment (see what changes occur naturally in the environment) - store it as state_0
     # 2) Take an action/reward - store as action_0 & reward_0
@@ -49,7 +67,7 @@ class LearningAgent(Agent):
         if self.iteration_0 == True:
             # TODO: Make this more elegant!!! And searchable!!
             self.q[0] = dict()
-            self.q[0] = {"state": {}, "actions": {}, "reward": {}}
+            self.q[0] = {"state": {}, "actions_rewards": {}}
             self.q[0]["state"] = dict()
             self.q[0]["state"] = { "inputs": inputs, "agent_action": self.next_waypoint}
             print "update(): Setting q[0][state] to {}".format(self.q[0]["state"])
@@ -64,7 +82,7 @@ class LearningAgent(Agent):
             self.q[0]["action"] = action
             self.q[0]["reward"] = reward
             print "update(): Setting q[0][action & reward] to {} & {}".format(self.q[0]["action"], self.q[0]["reward"])
-            self.iteration0 = False
+            self.iteration_0 = False
             # TODO - Save this state, etc. for the next iteration as prev_state
 
         # TODO: Learn policy based on state, action, reward

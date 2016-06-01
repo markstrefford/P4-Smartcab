c19eeef2c6d0fcb00ac637d6a96f72dbd659b631
Mark Strefford <mark@m20ventures.co.uk>
2016-06-01 23:04:35 +0100

    Improved state model
    Model now based on a state dict() and a qvalue() dict
    State provides a lookup that can point to values in qvalues()
    Hopefully this will make it easier!
    
    Also using tuples ((..), (..), (..)) for the state so that it is more
    easily searchable in a dict()
    
    Implemented the algorithm suggested in the forums whereby s0 is added,
    but we only store a0, r0 when we move to s1
    
    Hoping that this gets around the agent picking 'None' as an action every
    time like it did in my first version of the state model!!

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -13,42 +13,56 @@ class LearningAgent(Agent):
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
         self.possible_actions = [None, 'forward', 'left', 'right']
-        self.init_actions_rewards = dict({'None': self.set_initial_q(),
-                             'forward': self.set_initial_q(),
-                             'left': self.set_initial_q(),
-                             'right': self.set_initial_q()})
-        # self.states = self.setup_states()  # TODO - is this needed now?
-        self.q = dict()
-        self.iteration_0 = True
+        # self.init_actions_rewards = dict({'None': self.set_initial_q(),
+        #                      'forward': self.set_initial_q(),
+        #                      'left': self.set_initial_q(),
+        #                      'right': self.set_initial_q()})
+        self.states = dict()        # self.states[] is just the state key-value lookup table, so no actions or rewards here!!
+        self.q = dict()             # self.q[] is where we hold the actions and rewards, the state is the key from self.states[] (so typically an int!)
+        self.iteration = 0
+        self.prev_iteration = 0
+        self.prev_state, self.prev_action, self.prev_reward = {}, "", 0
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
-        self.iteration_0 = True  # TODO: Assume we do this every time for now...
+        self.iteration = 0  # TODO: Assume we do this every time for now...
+        self.prev_iteration = 0
+        self.prev_state = {}
+
+    def create_tuples(self, value):
+        return [(k, v) for (k, v) in value.iteritems()]
 
     def set_initial_q(self):
         #TODO - Determine optimal initialisation value
         return 0
 
-    # TODO - Make this work with multiple actions and rewards!
-    def new_state(self, state, structure = {"state": {}, "actions_rewards": {}}):
-        next_entry = len(self.q)
-        self.q[next_entry] = dict(structure)
-        self.q[next_entry]["state"] = state
-        self.q[next_entry]["actions_rewards"] = self.init_actions_rewards()
-        return next_entry
-
-    def set_state(self, id, state):
-        self.q[id]["state"] = state
-
-    def set_action(self, id, state, action, reward):
-        #TODO - Rework this whole thing!!
-        # Needs to find out if we can find a matching state first,
-        # create one if we don't
-        # Set the relevant action's reward (or modify it using q-learning eventually!)
-        self.q[id]["actions_rewards"] = action
+    def new_state(self, state):
+        print "new_state(): state = {}".format(state)
+        next_state_id = len(self.states)
+        self.states[next_state_id] = state
+        self.q[next_state_id] = dict({'None':self.set_initial_q(),
+                                      'forward':self.set_initial_q(),
+                                      'left':self.set_initial_q(),
+                                      'right':self.set_initial_q()})
+        return next_state_id
 
 
+    # This function just sets the s, a, and r
+    def update_qvalue(self, state, action, reward):
+        print "update_qvalue(): state = {}".format(state)
+        # 1st check whether we've seen this state before
+        if [ state in self.states ] == True:
+            for id, s in self.states.iteritems():
+                if state == s:
+                    self.q[id][action] = reward
+        else:
+            id = self.new_state(state)
+            self.q[id][action] = reward
+            # TODO - set up default q-values for actions, etc???
+        print "update_qvalue(): Updated the qvalues based on state {} id {}".format(state, id)
+
+    #
     # 1) Sense the environment (see what changes occur naturally in the environment) - store it as state_0
     # 2) Take an action/reward - store as action_0 & reward_0
     #
@@ -57,33 +71,40 @@ class LearningAgent(Agent):
     # 2) Update the Q-table using state_0, action_0, reward_0, state_1
     # 3) Take an action - get a reward
     # 4) Repeat
+    #
+    # So for iterations > 0 we need to see if the state exists, and if it does update the action/rewards accordingly
+    # and if it doesn't then create a new state with default action/rewards
+    # So I need to pass in a state (s), an action taken (a) and a reward (r) for taking that action
+    # But I am currently in a new state (s'), so really what is passed into update_qvalue() is the previous s, a, r
     def update(self, t):
+        print "update(): Iteration {}".format(self.iteration)
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
-        if self.iteration_0 == True:
-            # TODO: Make this more elegant!!! And searchable!!
-            self.q[0] = dict()
-            self.q[0] = {"state": {}, "actions_rewards": {}}
-            self.q[0]["state"] = dict()
-            self.q[0]["state"] = { "inputs": inputs, "agent_action": self.next_waypoint}
-            print "update(): Setting q[0][state] to {}".format(self.q[0]["state"])
+        self.state = (("lights", inputs['light']), ('oncoming', inputs['oncoming']), ('right', inputs['right']), ('left', inputs['left']), ("waypoint", self.next_waypoint))
+        if self.iteration == 0:
+            self.new_state(self.state)
+        else:
+            print "update(): Now updating state {}".format(self.iteration, self.prev_state)
+            # Now we know the reward for a specific action, we can update the previous state with what we know
+            self.update_qvalue(self.prev_state, self.prev_action, self.prev_reward)
 
         # TODO: Select action according to your policy
-        # action = None
         action = random.choice(self.possible_actions)
 
         # Execute action and get reward
         reward = self.env.act(self, action)
-        if self.iteration_0 == True:
-            self.q[0]["action"] = action
-            self.q[0]["reward"] = reward
-            print "update(): Setting q[0][action & reward] to {} & {}".format(self.q[0]["action"], self.q[0]["reward"])
-            self.iteration_0 = False
-            # TODO - Save this state, etc. for the next iteration as prev_state
+
+        # Save this state, etc. for the next iteration as prev_state
+        self.prev_iteration = self.iteration # So we can update the previous iteration (TODO - do we need this?)
+        self.iteration = self.iteration + 1  # Let's keep a count for now
+        self.prev_state = self.state
+        self.prev_action = action
+        self.prev_reward = reward
+
 
         # TODO: Learn policy based on state, action, reward
 
